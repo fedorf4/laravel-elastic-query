93f1d5980e5ff2e5ed85c0c16b4f9a554cc7d88c
MsNatali <MsNatali@users.noreply.github.com>
2024-06-26 11:50:00 +0000

    Fix styling

--- src/Contracts/DSL.php
+++ src/Contracts/DSL.php
@@ -8,6 +8,6 @@ class DSL
 {
     public static function filter(array $dsl): array|stdClass
     {
-        return array_filter($dsl, fn (mixed $item) => !is_null($item)) ?: new stdClass;
+        return array_filter($dsl, fn (mixed $item) => !is_null($item)) ?: new stdClass();
     }
 }
