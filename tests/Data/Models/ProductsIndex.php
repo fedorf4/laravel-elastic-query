acd7900b81f1f43ac5edbb2d358d8a6444260f7a
Ms_Natali <golubkova@greensight.ru>
2024-06-27 15:47:17 +0300

    CLOUDTECH-5 fix tests

--- tests/Data/Models/ProductsIndex.php
+++ tests/Data/Models/ProductsIndex.php
@@ -3,10 +3,21 @@
 namespace Ensi\LaravelElasticQuery\Tests\Data\Models;
 
 use Ensi\LaravelElasticQuery\ElasticIndex;
+use Illuminate\Support\Facades\ParallelTesting;
 
 class ProductsIndex extends ElasticIndex
 {
     protected string $name = 'test_products';
 
     protected string $tiebreaker = 'product_id';
+
+    protected function indexName(): string
+    {
+        return $this->name . (ParallelTesting::token() ?: 0);
+    }
+
+    public static function fullName(): string
+    {
+        return (new static())->indexName();
+    }
 }
