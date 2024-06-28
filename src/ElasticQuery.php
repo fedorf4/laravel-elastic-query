d4d9d1d753065b81d583a923b247817d5ba082d7
Ms_Natali <golubkova@greensight.ru>
2024-06-28 16:12:12 +0300

    CLOUDTECH-26 fix facade

--- src/ElasticQuery.php
+++ src/ElasticQuery.php
@@ -4,23 +4,24 @@ namespace Ensi\LaravelElasticQuery;
 
 use Elastic\Elasticsearch\Client;
 use Ensi\LaravelElasticQuery\Debug\QueryLogRecord;
+use Http\Promise\Promise;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Facade;
 
 /**
  * @method static Client getClient()
- * @method static array search(string $indexName, array $dsl)
- * @method static array searchAsync(string $indexName, array $dsl)
- * @method static array deleteByQuery(string $indexName, array $dsl)
- * @method static array get(string $indexName, int|string $id)
- * @method static array indicesExists(string $index)
- * @method static void indicesCreate(string $index, array $settings)
- * @method static array bulk(string $index, array $body)
- * @method static array documentDelete(string $index, int|string $id)
- * @method static array catIndices(string $indexName, array|null $getFields)
- * @method static array indicesDelete(string $indexName)
- * @method static array indicesRefresh(string $indexName)
- * @method static array indicesReloadSearchAnalyzers(string $indexName)
+ * @method static array|Promise search(string $indexName, array $dsl, string|null $searchType = null)
+ * @method static array|Promise deleteByQuery(string $indexName, array $dsl)
+ * @method static array|Promise get(string $indexName, int|string $id)
+ * @method static array|Promise indicesExists(string $index)
+ * @method static null|Promise indicesCreate(string $index, array $settings)
+ * @method static array|Promise bulk(string $index, array $body)
+ * @method static array|Promise documentDelete(string $index, int|string $id)
+ * @method static array|Promise catIndices(string $indexName, array|null $getFields = null)
+ * @method static array|Promise indicesInfo(array|null $indices = [], array $columns = ['i'], array $sort = [], string|null $health = null)
+ * @method static array|Promise indicesDelete(string $indexName)
+ * @method static array|Promise indicesRefresh(string $indexName)
+ * @method static array|Promise indicesReloadSearchAnalyzers(string $indexName)
  * @method static void enableQueryLog()
  * @method static void disableQueryLog()
  * @method static Collection|QueryLogRecord[] getQueryLog()
