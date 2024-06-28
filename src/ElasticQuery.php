4e199c9ff4456c7675ef4adaa0a43c95e5e544bb
Ms_Natali <golubkova@greensight.ru>
2024-06-28 16:14:39 +0300

    CLOUDTECH-26 fix facade

--- src/ElasticQuery.php
+++ src/ElasticQuery.php
@@ -4,20 +4,21 @@ namespace Ensi\LaravelElasticQuery;
 
 use Elasticsearch\Client;
 use Ensi\LaravelElasticQuery\Debug\QueryLogRecord;
+use GuzzleHttp\Ring\Future\FutureArray;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Facade;
 
 /**
  * @method static Client getClient()
- * @method static array search(string $indexName, array $dsl)
- * @method static array searchAsync(string $indexName, array $dsl)
+ * @method static array search(string $indexName, array $dsl, string|null $searchType = null)
+ * @method static FutureArray searchAsync(string $indexName, array $dsl, string|null $searchType = null)
  * @method static array deleteByQuery(string $indexName, array $dsl)
  * @method static array get(string $indexName, int|string $id)
  * @method static array indicesExists(string $index)
  * @method static void indicesCreate(string $index, array $settings)
  * @method static array bulk(string $index, array $body)
  * @method static array documentDelete(string $index, int|string $id)
- * @method static array catIndices(string $indexName, array|null $getFields)
+ * @method static array catIndices(string $indexName, array|null $getFields = null)
  * @method static array indicesDelete(string $indexName)
  * @method static array indicesRefresh(string $indexName)
  * @method static array indicesReloadSearchAnalyzers(string $indexName)
