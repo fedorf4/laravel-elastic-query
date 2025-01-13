fb3d14e2038a0478bbacc4cd2fc534cb6889f436
Fedor Fomichev <79131391+fedorf4@users.noreply.github.com>
2025-01-13 12:41:59 +0300

    Update InnerHits add array sorts

--- src/Search/Collapsing/InnerHits.php
+++ src/Search/Collapsing/InnerHits.php
@@ -12,6 +12,7 @@ class InnerHits implements DSLAware
         protected string $name,
         protected int $size,
         protected ?Sort $sort,
+        protected array $sorts = [],
     ) {
         Assert::stringNotEmpty(trim($name));
     }
@@ -25,6 +26,13 @@ class InnerHits implements DSLAware
 
         if ($this->sort) {
             $dsl['sort'] = $this->sort->toDSL();
+        }
+
+        if ($this->sorts) {
+            $dsl['sort'] = [];
+            foreach ($this->sorts as $sort) {
+                $dsl['sort'][] = $sort->toDSL();
+            }
         }
 
         return $dsl;
