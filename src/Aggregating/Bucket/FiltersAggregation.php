8231623427f356e59d21729760038b64217c46e6
Evgenij Manoylin <manoylin@greensight.ru>
2025-03-05 12:56:27 +0300

    IDBPR-2901 Add filters aggregation v8

---
+++ src/Aggregating/Bucket/FiltersAggregation.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Ensi\LaravelElasticQuery\Aggregating\Bucket;
+
+use Ensi\LaravelElasticQuery\Aggregating\BucketCollection;
+use Ensi\LaravelElasticQuery\Aggregating\FiltersCollection;
+use Ensi\LaravelElasticQuery\Aggregating\Result;
+use Ensi\LaravelElasticQuery\Contracts\Aggregation;
+use Webmozart\Assert\Assert;
+
+class FiltersAggregation implements Aggregation
+{
+    public function __construct(
+        private string $name,
+        private FiltersCollection $filters,
+        private ?Aggregation $composite = null,
+        private ?string $otherBucketKey = null,
+    ) {
+        Assert::stringNotEmpty(trim($name));
+        Assert::greaterThan($filters->count(), 0);
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function toDSL(): array
+    {
+        $body['filters']['filters'] = $this->filters->toDSL();
+
+        if ($this->otherBucketKey != null) {
+            $body['filters']['other_bucket_key'] = $this->otherBucketKey;
+        }
+
+        if ($this->isComposite()) {
+            $body['aggs'] = $this->composite->toDSL();
+        }
+
+        return [$this->name => $body];
+    }
+
+    public function parseResults(array $response): array
+    {
+        $buckets = array_map(
+            function (mixed $key, array $bucket) {
+                $values = $this->isComposite() ? $this->composite->parseResults($bucket) : [];
+
+                return Result::parseBucketWithKey($key, $bucket, $values);
+            },
+            array_keys($response[$this->name]['buckets'] ?? []),
+            $response[$this->name]['buckets'] ?? []
+        );
+
+        return [$this->name => new BucketCollection($buckets)];
+    }
+
+    public function isComposite(): bool
+    {
+        return isset($this->composite);
+    }
+}
